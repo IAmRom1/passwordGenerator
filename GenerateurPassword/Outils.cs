9afcf4467f4d40381e0169e927559ad3c5c71bd8
Romain <git@romainsavigny.be>
2024-12-08 12:59:13 +0100

    SETUP: initation des élements de base

---
+++ GenerateurPassword/Outils.cs
@@ -0,0 +1,50 @@
+﻿namespace FormationCS;
+
+// FormationCS.Outils.DemanderNombrePositifNonNull
+
+static class Outils
+{
+    public static int DemanderNombrePositifNonNull(string question)
+    {
+        int nombre = DemanderNombre(question);
+
+        if (nombre >= 1)
+        {
+            return nombre;
+        }
+        Console.WriteLine("Le chiffre ne pas pas être négatif et nul");
+        return DemanderNombreEntre(question, 1, int.MaxValue);
+    }
+    
+    static int DemanderNombreEntre(string question, int min, int max)
+    {
+        int nombre = DemanderNombre(question);
+
+        if (nombre >= min && nombre <= max)
+        {
+            return nombre;
+        }
+        Console.WriteLine($"Le nombre ne peu que être entre {min} et {max}");
+
+        return DemanderNombreEntre(question, min, max);
+    }
+    
+    static int DemanderNombre(string question)
+    {
+        while (true)
+        {
+            Console.Write(question);
+            string nombre_str = Console.ReadLine();
+
+            try
+            {
+                int nombre = int.Parse(nombre_str);
+                return nombre;
+            }
+            catch
+            {
+                Console.WriteLine("Vous devez saisir un nombre uniquement !"); 
+            }
+        } 
+    }
+}
---
+++ GenerateurPassword/Program.cs
@@ -0,0 +1,43 @@
+﻿using FormationCS;
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        // 1 - demander la length du password (demander nombre)
+       // int longeurDuPassword = Outils.DemanderNombrePositifNonNull("Longeur du mot de passe :");
+        
+       // Console.WriteLine(longeurDuPassword);
+
+        // 2 - alphabet "abcd1234"
+        // 3 - comment choisir un caractère random
+        
+        // for avec la longeur qui est longeurDuPassword 
+            // chiffre random 
+            // string[rng]
+            // += password
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string chiffre = "1234567890";
+            string caractereSpeciaux = "!@#$%^&*()_+-=[]{}|;:'\",.<>?/`~\\";
+            
+            
+            string password = "";
+            for (int n = 0; n <= 3; n++)
+            {
+                for (int i = 0; i < 8 ; i++)
+                {
+                    Random rand = new Random();
+                    int randomAlphabet = rand.Next(1, alphabet.Length);
+                    int randomChiffre = rand.Next(1, chiffre.Length);
+                    int randomCaractereSpeciaux = rand.Next(1, caractereSpeciaux.Length);
+                    
+                    password += alphabet[randomAlphabet];
+                }
+                Console.WriteLine(password);
+                password = "";
+            }
+        
+        // 4 - generer le mot de passe
+        // 5 - chois alphabet
+    }
+}
